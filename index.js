715185fdfd87f5e3c05f8f54410c406211fd0380
mdacht777 <mdacht777@gmail.com>
2020-10-26 17:55:44 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(arg,sel){
+    return arg.filter(s => s.toLowerCase() == sel.toLowerCase());
+}
+
+function fuzzyMatch(arg,sel){
+    return arg.filter(s => s.charAt(0) == sel.charAt(0));
+}
+
+function matchName(arg,sel){
+    return arg.filter(s => s.name == sel);
+}
